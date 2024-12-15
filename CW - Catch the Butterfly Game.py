bcb8eb6e908b62f952f66c269d1a3143274d056a
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2024-12-15 10:30:55 +0000

    Add files via upload

--- CW - Catch the Butterfly Game.py
+++ CW - Catch the Butterfly Game.py
@@ -1,16 +1,29 @@
 import pgzrun
+import random
 
 WIDTH = 800
 HEIGHT = 450
 TITLE = 'Catch the Butterfly Game'
 
+score = 0
+
 butterfly = Actor('butterfly')
 butterfly.pos = (400, 225)
+
+def moveButterfly():
+    butterfly.pos = (random.randint(50, 750), random.randint(50, 200))
+
+def on_mouse_down(pos):
+    global score
+    if butterfly.collidepoint(pos):
+        moveButterfly()
+        score = score + 1
 
 def draw():
     screen.clear()
     screen.blit('background', (0, 0))
     butterfly.draw()
+    screen.draw.text('Score: ' + str(score), (50, 50))
 
 
 
